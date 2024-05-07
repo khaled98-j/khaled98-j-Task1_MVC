1bc974f0ddda180bc878fe3be31e82ffeb07971b
khaled98-j <khjabarin6@gmail.com>
2024-05-07 16:57:23 +0300

    CRUD

--- WebApplication1/Controllers/EmployeeController.cs
+++ WebApplication1/Controllers/EmployeeController.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using WebApplication1.Data;
+using WebApplication1.Models;
 
 namespace WebApplication1.Controllers
 {
@@ -12,5 +13,56 @@ namespace WebApplication1.Controllers
             var employee = context.Employees.ToList();
             return View("Index",employee);
         }
+
+        public IActionResult Details(int id)
+        {
+            //return Content($"id = {id}");
+
+            var employee = context.Employees.Find(id);
+            return View("Details", employee);
+        }
+        
+        public IActionResult Create()
+        {
+            
+            return View("Create");
+        }
+
+        public IActionResult Store(Employee employee)
+        {
+            var changeEmployee = context.Employees.Find(employee.Id);
+            if (changeEmployee == null)
+            {
+                context.Employees.Add(employee);
+                
+            }
+            else
+            {
+                changeEmployee.Name = employee.Name;
+                changeEmployee.Title = employee.Title;
+                changeEmployee.Age   = employee.Age;
+            
+            }
+
+            context.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        public IActionResult Delete(int id)
+        {
+            var employee = context.Employees.Find(id);  
+            context.Employees.Remove(employee);
+            context.SaveChanges();
+
+            return RedirectToAction("Index");
+        }
+
+        public IActionResult Update(int id)
+        {
+            var employee = context.Employees.Find(id);
+            return View("Update", employee);
+        }
+
+
     }
 }
--- WebApplication1/obj/Release/net7.0/WebApplication1.AssemblyInfo.cs
+++ WebApplication1/obj/Release/net7.0/WebApplication1.AssemblyInfo.cs
@@ -12,7 +12,7 @@ using System;
 using System.Reflection;
 
 [assembly: System.Reflection.AssemblyCompanyAttribute("WebApplication1")]
-[assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
+[assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
 [assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e6ae9e2bcfb2a314be43dc69270e41ccd2e5af40")]
 [assembly: System.Reflection.AssemblyProductAttribute("WebApplication1")]
